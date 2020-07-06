6c978798f25d799bf9df2aefe2447442b689b002
evamera <eva@evamera>
2020-07-06 14:51:01 +0200

    private area login

--- PetsAppDot/routes/guestprivatearea.routes.js
+++ PetsAppDot/routes/guestprivatearea.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const passport = require("passport");
 
 // Role checker middleware
-router.get('/profile', (req, res) => res.send('inicio de area privada de association'))
+router.get('/profile', (req, res) => res.send('inicio de area privada de guest'))
 
 module.exports = router;
--- PetsAppDot/routes/index.js
+++ PetsAppDot/routes/index.js
@@ -1,8 +1,13 @@
 module.exports = app => {
 
+    // Role checker middleware
+    const checkRole = rolesToCheck => (req, res, next) => req.isAuthenticated() && rolesToCheck.includes(req.user.role) ? next() : res.redirect('/login')
+
+
     // Base URLS
     app.use('/', require('./index.routes'))
     app.use('/', require('./auth.routes'))
     app.use('/', require('./roles.routes'))
-    app.use('/', require('./assprivatearea.routes'))
+    app.use('/asociacion', checkRole(['ASSOCIATIONS', 'ADMIN']), require('./assprivatearea.routes'))
+    app.use('/guest', checkRole(['GUEST', 'ADMIN']), require('./guestprivatearea.routes'))
 }
